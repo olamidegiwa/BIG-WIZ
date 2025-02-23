c67b96827b8dcec230a8ee4a2effa57945c3334d
olamidegiwa <giwasodiq7@gmail.com>
2025-02-23 01:03:30 +0100

    updated

--- src/Popup/VideoPopup.jsx
+++ src/Popup/VideoPopup.jsx
@@ -15,66 +15,8 @@ const VideoPopup = () => {
         <h1 className="text-start custom-text-white pb-lg-5 pb-2 px-0">
           MORE VIDEOS FROM WIZKID
         </h1>
-        {/* <div className="media-container">
-          {Videos.map((video) => {
-            return (
-              <div key={video.id}>
-                <img
-                  onClick={() => setSelectedVideo(video.url)}
-                  src={video.image}
-                  alt="image"
-                  data-bs-toggle="modal"
-                  className="media cursor-pointer"
-                  data-bs-target="#imageExample"
-                />
-
-                <div className="d-flex justify-content-between">
-                  <p className="custom-text-white title">{video.desc}</p>
-
-            
-
-                  <div onClick={() => setSelectedVideo(video.url)}>
-                    <a
-                      target="_blank"
-                      className="custom-text-white title"
-                      href={video.url}
-                    >
-                      WATCH NOW
-                    </a>
-                  </div>
-                </div>
-
-                <div
-                  className="modal fade  "
-                  id="imageExample"
-                  tableIndex="-1"
-                  aria-hidden="true"
-                >
-                  <div className="modal-dialog">
-                    <div className="modal-content">
-                      <div className="modal-body">
-                        <ReactPlayer
-                          url={selectedVideo}
-                          controls={true}
-                          className="d-block video"
-                        />
-                      </div>
-
-                      <button
-                        type="button"
-                        className="custom-bg-black  custom-text-white text-decoration-underline border-0 m-0 modal-button"
-                        data-bs-dismiss="modal"
-                      >
-                        CLOSE
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div> */}
-        <di className="media-container">
+        
+        <div className="media-container">
           {Videos.map((video) => {
             return (
               <div key={video.id}>
@@ -132,7 +74,7 @@ const VideoPopup = () => {
               </div>
             );
           })}
-        </di>
+        </div>
       </div>
     </div>
   );
